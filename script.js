09a99c488bac315f76988d831c257fdc2d1aa1db
Hatim Patanwala <hatim@beatroute.io>
2025-07-22 10:49:45 +0530

    image remove

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('images-container');
-  const imageList = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg'];
-
+  // const imageList = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg'];
+  const imageList = [];
   imageList.forEach((img) => {
     const image = document.createElement('img');
     image.src = `images/${img}`;
